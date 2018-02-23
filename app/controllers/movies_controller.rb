df874b87cc631319cd313f273aa55c90438b58f2
Jarmo <jarmo@julianus.ee>
2018-02-23 12:30:34 +0200

    Movie sort by genre

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
 
   def index
-    movies = Movie.all
+    if params[:genre] and params[:genre] != "0"
+      movies = Movie.joins(:genres).where('genres.id = ?', params[:genre])
+    else
+      movies = Movie.all
+    end
+
     render json: movies
   end
 
